c18e92bdac77062a0f2f4af452770b4fafaf30c1
jteng01 <joshuateng01@gmail.com>
2025-05-15 23:09:45 -0700

    Checking for best frame
    Refactored the DNAReader to check each frame in forward and reverse compliment genes using a hashmap, index lists, and double pointer approach to print out the best frame.

--- src/DNAReader.java
+++ src/DNAReader.java
@@ -1,53 +1,176 @@
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class DNAReader {
     private String dna;
+    private String dnaReverseComplement;
 
     public DNAReader(String dna){
         this.dna = dna.toUpperCase();
+        this.dnaReverseComplement = computeReverseComplement(this.dna);
     }
 
-    public ArrayList<String> locateGenes(){
+    public Map<Integer, List<Integer>> locateATGIndices(String dnaInput) {
+        Map<Integer, List<Integer>> atgIndices = new HashMap<>();
+        for (int frame = 0; frame < 3; frame++) {
+            atgIndices.put(frame, new ArrayList<>());
+        }
+
         int atgIndex = 0;
-        ArrayList<String> genes = new ArrayList<>();
+        while (true) {
+            atgIndex = dnaInput.indexOf("ATG", atgIndex);
+            if (atgIndex == -1) break;
 
-        while(true){
-            atgIndex = dna.indexOf("ATG", atgIndex);
-            if(atgIndex == -1){break;}
+            int frame = atgIndex % 3;
+            atgIndices.get(frame).add(atgIndex);
+            atgIndex += 1;
+        }
 
-            int stopIndex = findStopCodon(dna, atgIndex);
-            if(stopIndex != -1) {
-                genes.add( "Frame offset: " + (atgIndex % 3) + " | Gene: " + dna.substring(atgIndex, stopIndex + 3) + " | Index: " + atgIndex);
+        return atgIndices;
+    }
+
+    public Map<Integer, List<Integer>> locateSortedStopCodonIndices(String dna) {
+        Map<Integer, List<Integer>> stopCodonIndices = new HashMap<>();
+        for (int frame = 0; frame < 3; frame++) {
+            stopCodonIndices.put(frame, new ArrayList<>());
+        }
+
+        String[] stopCodons = {"TAA", "TAG", "TGA"};
+
+        for (String codon : stopCodons) {
+            int index = 0;
+            while ((index = dna.indexOf(codon, index)) != -1) {
+                int frame = index % 3;
+                stopCodonIndices.get(frame).add(index);
+                index += 1;
+            }
+        }
+
+        for (List<Integer> indices : stopCodonIndices.values()) {
+            indices.sort(Integer::compareTo);
+        }
+
+        return stopCodonIndices;
+    }
+
+    public Map<Integer, List<Entry<Integer, Integer>>> getStartStopPairsNoOverlapByFrame(
+            Map<Integer, List<Integer>> startCodonsByFrame,
+            Map<Integer, List<Integer>> stopCodonsByFrame) {
+
+        Map<Integer, List<Entry<Integer, Integer>>> pairsByFrame = new HashMap<>();
+
+        for (int frame = 0; frame < 3; frame++) {
+            List<Integer> startCodons = startCodonsByFrame.getOrDefault(frame, new ArrayList<>());
+            List<Integer> stopCodons = stopCodonsByFrame.getOrDefault(frame, new ArrayList<>());
+
+            List<Entry<Integer, Integer>> pairs = new ArrayList<>();
+            int stopPtr = 0;
+            int lastGeneEnd = -1;
+
+            for (int start : startCodons) {
+                if (start < lastGeneEnd) {
+                    continue;
+                }
+
+                while (stopPtr < stopCodons.size() && stopCodons.get(stopPtr) <= start) {
+                    stopPtr++;
+                }
+
+                while (stopPtr < stopCodons.size()) {
+                    int stop = stopCodons.get(stopPtr);
+                    if ((stop - start) % 3 == 0) {
+                        pairs.add(new SimpleEntry<>(start, stop));
+                        lastGeneEnd = stop + 3;
+                        break;
+                    }
+                    stopPtr++;
+                }
             }
 
-            atgIndex = atgIndex + 1;
-
+            pairsByFrame.put(frame, pairs);
         }
+
+        return pairsByFrame;
+    }
+
+    public ArrayList<String> locateGenes() {
+        Map<Integer, List<Integer>> startForward = locateATGIndices(dna);
+        Map<Integer, List<Integer>> stopForward = locateSortedStopCodonIndices(dna);
+        Map<Integer, List<Entry<Integer, Integer>>> pairsForward =
+                getStartStopPairsNoOverlapByFrame(startForward, stopForward);
+
+        Map<Integer, List<Integer>> startReverse = locateATGIndices(dnaReverseComplement);
+        Map<Integer, List<Integer>> stopReverse = locateSortedStopCodonIndices(dnaReverseComplement);
+        Map<Integer, List<Entry<Integer, Integer>>> pairsReverse =
+                getStartStopPairsNoOverlapByFrame(startReverse, stopReverse);
+
+        int maxPairs = -1;
+        int bestFrame = -1;
+        String bestStrand = "";
+        List<Entry<Integer, Integer>> bestPairs = null;
+
+        for (Map.Entry<Integer, List<Entry<Integer, Integer>>> entry : pairsForward.entrySet()) {
+            if (entry.getValue().size() > maxPairs) {
+                maxPairs = entry.getValue().size();
+                bestFrame = entry.getKey();
+                bestStrand = "Forward";
+                bestPairs = entry.getValue();
+            }
+        }
+
+        for (Map.Entry<Integer, List<Entry<Integer, Integer>>> entry : pairsReverse.entrySet()) {
+            if (entry.getValue().size() > maxPairs) {
+                maxPairs = entry.getValue().size();
+                bestFrame = entry.getKey();
+                bestStrand = "Reverse";
+                bestPairs = entry.getValue();
+            }
+        }
+
+        ArrayList<String> genes = new ArrayList<String>();
+        if (bestPairs == null || bestPairs.isEmpty()) {
+            return genes;
+        }
+
+        genes.add(String.format("Frame %d (%s):", bestFrame, bestStrand));
+
+        for (Entry<Integer, Integer> pair : bestPairs) {
+            int start = pair.getKey();
+            int stop = pair.getValue();
+            String geneSeq;
+
+            if (bestStrand.equals("Forward")) {
+                geneSeq = dna.substring(start, stop + 3);
+            } else {
+                geneSeq = dnaReverseComplement.substring(start, stop + 3);
+                geneSeq = computeReverseComplement(geneSeq);
+            }
+
+            genes.add(geneSeq);
+        }
+
         return genes;
     }
 
-    public int findStopCodon(String dna, int startIndex){
-        int stopIndex = Integer.MAX_VALUE;
-        boolean found = false;
 
-        for(String codon : new String[]{"TAA", "TAG", "TGA"}){
-            int compareIndex = dna.indexOf(codon, startIndex + 3);
-            while (compareIndex != -1) {
-                if ((compareIndex - startIndex) % 3 == 0) {
-                    if (compareIndex < stopIndex) {
-                        stopIndex = compareIndex;
-                        found = true;
-                    }
-                    break;
-                }
-                compareIndex = dna.indexOf(codon, compareIndex + 1);
+    private String computeReverseComplement(String seq) {
+        StringBuilder revComp = new StringBuilder();
+        for (int i = seq.length() - 1; i >= 0; i--) {
+            char base = seq.charAt(i);
+            switch (base) {
+                case 'A': revComp.append('T'); break;
+                case 'T': revComp.append('A'); break;
+                case 'C': revComp.append('G'); break;
+                case 'G': revComp.append('C'); break;
+                default: revComp.append('N');
             }
         }
-        if(!found){
-            stopIndex = -1;
-        }
-
-        return stopIndex;
+        return revComp.toString();
     }
+
 
 }
