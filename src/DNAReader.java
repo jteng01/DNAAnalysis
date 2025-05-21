84ffcf11b4bd89e861acca52656af654ee263567
jteng01 <joshuateng01@gmail.com>
2025-05-20 19:23:07 -0700

    fixed DNA Reader

--- src/DNAReader.java
+++ src/DNAReader.java
@@ -8,21 +8,33 @@ public class DNAReader {
     }
 
     public ArrayList<String> locateGenes(){
+        ArrayList<String> genes = new ArrayList<>();
+
+        genes.addAll(locateGenesInStrand(dna, false));
+
+        String reverseComp = getReverseComplement(dna);
+        genes.addAll(locateGenesInStrand(reverseComp, true));
+
+        return genes;
+    }
+
+    private ArrayList<String> locateGenesInStrand(String strand, boolean isReverse) {
         int atgIndex = 0;
         ArrayList<String> genes = new ArrayList<>();
 
         while(true){
-            atgIndex = dna.indexOf("ATG", atgIndex);
+            atgIndex = strand.indexOf("ATG", atgIndex);
             if(atgIndex == -1){break;}
 
-            int stopIndex = findStopCodon(dna, atgIndex);
+            int stopIndex = findStopCodon(strand, atgIndex);
             if(stopIndex != -1) {
-                genes.add( "Gene: " + dna.substring(atgIndex, stopIndex + 3) + " | Index: " + atgIndex);
+                genes.add("Gene: " + strand.substring(atgIndex, stopIndex + 3) + " | Index: " + atgIndex + " | Reverse: " + isReverse
+                );
             }
 
-            atgIndex = stopIndex + 1;
-
+            atgIndex = (stopIndex != -1) ? stopIndex + 1 : atgIndex + 3;
         }
+
         return genes;
     }
 
@@ -43,11 +55,23 @@ public class DNAReader {
                 compareIndex = dna.indexOf(codon, compareIndex + 1);
             }
         }
-        if(!found){
-            stopIndex = -1;
-        }
-
-        return stopIndex;
+        return found ? stopIndex : -1;
     }
 
+    public String getReverseComplement(String sequence){
+        StringBuilder reverseComp = new StringBuilder();
+
+        for (int i = sequence.length() - 1; i >= 0; i--) {
+            char base = sequence.charAt(i);
+            switch(base){
+                case 'A': reverseComp.append('T'); break;
+                case 'T': reverseComp.append('A'); break;
+                case 'C': reverseComp.append('G'); break;
+                case 'G': reverseComp.append('C'); break;
+                default: reverseComp.append('N');
+            }
+        }
+
+        return reverseComp.toString();
+    }
 }
